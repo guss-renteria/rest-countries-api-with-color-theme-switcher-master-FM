1b0773ec94a87bd9795271ffafcb7068dc5bd981
guss-renteria <gusstavo.renteria.g@gmail.com>
2022-06-21 02:36:01 -0500

    se agrego diseño a 'NotFound' y 'Loading'

--- src/views/loading/loading.view.jsx
+++ src/views/loading/loading.view.jsx
@@ -1,6 +1,12 @@
+import { useSelector } from 'react-redux'
+
+import './_loading.style.scss'
+
 const Loading = () => {
+  const theme = useSelector(state => state.color_scheme.theme.name)
+
   return (
-    <section className='loading-view'>
+    <section className={ `loading-view ${ theme }` }>
       <h1>loading...</h1>
     </section>
   )
--- src/views/not_found/not_found.view.jsx
+++ src/views/not_found/not_found.view.jsx
@@ -1,7 +1,13 @@
+import { useSelector } from 'react-redux'
+
+import './_not_found.style.scss'
+
 const NotFound = () => {
+  const theme = useSelector(state => state.color_scheme.theme.name)
+
   return (
-    <section className="not-found-view">
-      <h1>not found</h1>
+    <section className={ `not-found-view ${ theme }` }>
+      <h1>not found :x</h1>
     </section>
   )
 }
